6032f5e58f99ae5bbcee886b74cea08f955f669d
purplegem903 <82189117+purplegem903@users.noreply.github.com>
2023-03-20 05:50:50 +0100

    python formatter.black

--- week-9-flask/current-time/app.py
+++ week-9-flask/current-time/app.py
@@ -4,16 +4,19 @@ from pytz import timezone
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def time():
     cambridge_time = timezone("America/New_York")
     now = datetime.now(cambridge_time)
-    date_string = now.strftime("%A, %B %d, %Y") # format the date as "Monday, January 01, 2022"
+    # format the date as "Monday, January 01, 2022"
+    date_string = now.strftime("%A, %B %d, %Y")
     eastern_time = timezone("America/New_York")
     amsterdam = timezone("Europe/Amsterdam")
     now = datetime.now(eastern_time)
     now2 = datetime.now(amsterdam)
-    return render_template("index.html",date=date_string, now=now, now2=now2) 
+    return render_template("index.html", date=date_string, now=now, now2=now2)
+
 
 if __name__ == "__main__":
     app.run(debug=True)

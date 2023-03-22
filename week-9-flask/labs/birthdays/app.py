eddba06ffcc54ed602f3dcd5d2bb27b14bfc5bd8
purplegem903 <82189117+purplegem903@users.noreply.github.com>
2023-03-22 02:33:32 +0100

    birthday edit.

--- week-9-flask/labs/birthdays/app.py
+++ week-9-flask/labs/birthdays/app.py
@@ -5,6 +5,8 @@ from flask import Flask, flash, jsonify, redirect, render_template, request, ses
 
 # Configure application
 app = Flask(__name__)
+app.secret_key = 'your-secret-key-here'
+
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -36,21 +38,97 @@ def after_request(response):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # TODO: Add the user's entry into the database
+        # Validate user input
         name = request.form.get("name")
-        month = request.form.get("month")
-        day = request.form.get("day")
+        if not name or len(name) > 50 or not name.isalpha():
+            flash(
+                "Invalid name format. Please enter a string with maximum 50 characters.")
+            return redirect("/")
+
+        try:
+            month = int(request.form.get("month"))
+            if not 1 <= month <= 12:
+                raise ValueError()
+        except (ValueError, TypeError):
+            flash(
+                "Invalid month value. Please enter a positive integer between 1 and 12.")
+            return redirect(request.url)
+
+        try:
+            day = int(request.form.get("day"))
+            if not 1 <= day <= 31:
+                raise ValueError()
+        except (ValueError, TypeError):
+            flash("Invalid day value. Please enter a positive integer between 1 and 31.")
+            return redirect(request.url)
+
+        # Add the user's entry into the database
         new_birthday = Birthdays(name=name, month=month, day=day)
         db.session.add(new_birthday)
         db.session.commit()
-
         return redirect("/")
+
+    edit_id = request.args.get("edit_id")
+    if edit_id:
+        # Retrieve the corresponding entry from the database
+        birthday = Birthdays.query.get(edit_id)
+        if birthday:
+            # Display the edit form for the entry
+            return render_template("index.html", birthday=birthday, edit_mode=True)
+        else:
+            flash("Invalid birthday ID.")
 
     else:
 
         # TODO: Display the entries in the database on index.html
         birthdays = Birthdays.query.all()
         return render_template("index.html", birthdays=birthdays)
+
+
+@app.route("/delete/<int:id>", methods=["POST"])
+def delete(id):
+    birthday = Birthdays.query.get_or_404(id)
+    db.session.delete(birthday)
+    db.session.commit()
+    flash("Birthday entry deleted successfully.")
+    return redirect("/")
+
+
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    birthday = Birthdays.query.get_or_404(id)
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+
+        # Validate input
+        errors = []
+        if not name or len(name) > 50:
+            errors.append(
+                "Name should be a string with maximum 50 characters.")
+        if not month or not month.isdigit() or int(month) < 1 or int(month) > 12:
+            errors.append(
+                "Month should be a positive integer between 1 and 12.")
+        if not day or not day.isdigit() or int(day) < 1 or int(day) > 31:
+            errors.append("Day should be a positive integer between 1 and 31.")
+
+        # If input is valid, update the entry and redirect to home page
+        if not errors:
+            birthday.name = name
+            birthday.month = month
+            birthday.day = day
+            db.session.commit()
+            flash("Birthday updated successfully.")
+            return redirect("/")
+
+        # If input is not valid, show error messages and reload the form
+        for error in errors:
+            flash(error)
+
+    # Render the edit form
+    return render_template("index.html", edit_mode=True, bday=birthday)
 
 
 if __name__ == '__main__':
